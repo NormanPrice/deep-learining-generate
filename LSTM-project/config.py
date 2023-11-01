245d82b0a68d5b647db13152348714f39cfcb4f4
NormanPrice <84268827+NormanPrice@users.noreply.github.com>
2023-11-01 20:49:45 +0100

    Delete LSTM-project/config.py

--- LSTM-project/config.py
+++
@@ -1,8 +0,0 @@
-load_saved_model = False
-train_model = True
-seq_length = 20
-filename = "/content/drive/MyDrive/rockyou-75.txt"
-model_path = '/content/drive/MyDrive/model-saveLSTM.h5'
-output_file = 'LSTM-output.txt'
-total_epochs = 100
-batch_size = 32
