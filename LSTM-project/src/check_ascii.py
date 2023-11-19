fa697e0b8e4e98dcd7d159e7de6c694bee19256c
NormanPrice <84268827+NormanPrice@users.noreply.github.com>
2023-11-19 18:20:06 +0100

    Update check_ascii.py

--- LSTM-project/src/check_ascii.py
+++ LSTM-project/src/check_ascii.py
@@ -1,18 +1,20 @@
 def remove_non_ascii_printable(file_path):
     """
-    Remove non-ASCII printable characters from a text file.
+    Remove non-ASCII printable characters from a text file while preserving its structure.
 
     :param file_path: Path to the text file to be processed.
     :return: True if the file was processed successfully, False otherwise.
     """
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             text = file.read()
 
-        # Filter out non-ASCII printable characters (ASCII range: 32 to 126 inclusive)
-        filtered_text = ''.join(char for char in text if 32 <= ord(char) <= 126)
+        # Filter out non-ASCII printable characters, but keep line breaks and other whitespaces
+        # ASCII range for printable characters: 32 to 126
+        # ASCII codes for newline (10) and carriage return (13)
+        filtered_text = ''.join(char for char in text if 32 <= ord(char) <= 126 or ord(char) in [10, 13])
 
-        with open(file_path, 'w') as file:
+        with open(file_path, 'w', encoding='utf-8') as file:
             file.write(filtered_text)
 
         return True
@@ -20,11 +22,11 @@ def remove_non_ascii_printable(file_path):
         print(f"An error occurred: {e}")
         return False
 
-# Example usage:
-file_path = '/Users/norbert/Downloads/deep-learining-generate-main/LSTM-project/data/10-million-password-list-top-100000.txt'
+'''# Example usage:
+file_path = '/Users/norbert/Downloads/deep-learining-generate-main/LSTM-project/data/darkweb2017.txt'
 result = remove_non_ascii_printable(file_path)
 if result:
     print("Non-ASCII printable characters have been removed.")
 else:
     print("An error occurred during processing.")
-
+'''
