d856923b562ac4e1641cc145bd9da2ca7a97b622
NormanPrice <84268827+NormanPrice@users.noreply.github.com>
2023-11-19 19:26:55 +0100

    Update config.py

--- LSTM-project/src/config.py
+++ LSTM-project/src/config.py
@@ -1,10 +1,10 @@
 import sys, os
 seq_length = 20
-filename = "/Users/norbert/Downloads/deep-learining-generate-main/LSTM-project/data/rockyou-75.txt"
+filename = "/data/rockyou-75.txt"
 model_save_path = "models/model-saveLSTM-91-chars.h5"
 output_path = "outputs/LSTM-output-new.txt"
 load_saved_model=True
 train_model = False
-chars_path = "/Users/norbert/Downloads/deep-learining-generate-main/LSTM-project/data/ascii_printable_characters.txt"
+chars_path = "/data/ascii_printable_characters.txt"
 epochs = 10
 batch_size = 64
